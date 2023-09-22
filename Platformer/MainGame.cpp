b7f09edfabf650f05dd83459f8292a0eb5f2a951
AdiPun <adittyapun11@gmail.com>
2023-09-22 16:58:41 +0100

    Added load background

--- Platformer/MainGame.cpp
+++ Platformer/MainGame.cpp
@@ -6,6 +6,7 @@ void Draw();
 void MainGameEntry(PLAY_IGNORE_COMMAND_LINE)
 {
 	Play::CreateManager(DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE);
+	Play::LoadBackground("Data\\Backgrounds\\background.png");
 
 }
 
