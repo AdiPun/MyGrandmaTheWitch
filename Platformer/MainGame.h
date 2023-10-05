ce33586502dc09f8aa17943732eff3a6e6be928e
AdiPun <adittyapun11@gmail.com>
2023-10-05 15:33:03 +0100

    Moving things to MainGame.h

--- Platformer/MainGame.h
+++ Platformer/MainGame.h
@@ -5,3 +5,155 @@
 const int DISPLAY_WIDTH = 1280;
 const int DISPLAY_HEIGHT = 720;
 const int DISPLAY_SCALE = 1;
+
+enum GameObjectType
+{
+	TYPE_PLAYER,
+	TYPE_PLATFORM,
+	TYPE_BACKGROUND,
+};
+
+enum PlayerState
+{
+	STATE_IDLE = 0,
+	STATE_RUNNING,
+	STATE_SLIDING,
+	STATE_JUMPING,
+	STATE_JUMPINGDOWN,
+	STATE_FALLING,
+	STATE_LANDING,
+	STATE_ATTACK,
+};
+
+struct PlayerInfo
+{
+	Vector2D verticalcollisionAABB{ 15,30 };
+	Vector2D wallcollisionAABB{ 15,20 };
+	Vector2D slidingAABB{ 15,0 };
+	Vector2D standingAABB{ 15,20 };
+	Vector2D damageAABB{ 5,10 };
+
+	Vector2D headboxAABB{ 15,1 };
+	Vector2D headboxoffset{ 0,25 };
+
+
+	Vector2D PlatformToPlayerDistance;
+
+	bool facingright;
+	float animationspeedidle{ 0.2f };
+	float animationspeedrun{ 0.2f };
+	float animationspeedjump{ 0.2f };
+	float animationspeedfall{ 0.2f };
+	float animationspeedland{ 0.2f };
+	float animationspeedatk{ 0.2f };
+
+	float friction;
+	float slidingfriction{ 0.8f };
+	float runningandjumpingfriction{ 0.8f };
+
+
+	float runspeed{ 4.5f };
+	float slidespeedCounter{ 1.2f };
+	float slidetimer{ 2.0f };
+	const float slidespeed{ 1.2f };
+	float jumpspeed{ -10.0f };
+	float fallspeed{ 3.5f };
+	const float terminalvelocity{ 6.0f };
+
+	float scale{ 2.0f };
+	float gravity{ 0.3f };
+
+	bool playerleftofplatform;
+	bool headboxleftofplatform;
+};
+
+
+
+struct VariableJump
+{
+	float jumpStartTime;
+	float jumpTime;
+	bool isJumping;
+};
+
+struct CoyoteJump
+{
+	const float coyoteTime = 0.2f;
+	float coyoteTimeCounter;
+};
+
+struct JumpBuffer
+{
+	const float jumpbufferTime = 0.2f;
+	float jumpbufferTimeCounter;
+};
+
+struct Platform
+{
+	int type = TYPE_PLATFORM;
+	Point2D pos;
+	Vector2D AABB{ 32,32 };
+
+};
+
+struct PlatformInfo
+{
+	Point2D CeilingCollidedPos;
+	int levelheight;
+	int levelwidth;
+};
+
+
+struct GameState
+{
+	float elapsedTime = 0;
+	PlayerState playerstate = STATE_JUMPINGDOWN;
+	std::vector<Platform> vPlatforms;
+};
+
+VariableJump variablejump;
+CoyoteJump coyotejump;
+JumpBuffer jumpbuffer;
+PlayerInfo playerinfo;
+Platform platform;
+PlatformInfo platforminfo;
+GameState gamestate;
+
+
+void UpdatePlayer();
+void HandleGroundedControls();
+
+void HandleSlidingControls();
+
+void HandleAirBorneControls();
+
+void HandleFallingControls();
+void HandleGroundedAttackControls();
+
+
+void CreatePlatform(int x, int y);
+void CreatePlatformRow(int tiles, int x, int y);
+void CreatePlatformColumn(int tiles, int x, int y);
+void CreatePlatformFloor();
+
+bool IsGrounded();
+bool FloorCollisionStarted();
+bool CeilingCollisionStarted();
+bool IsUnderCeiling();
+bool WillCollideWithWall();
+bool IsInsideWall();
+void CheckPlayerIsLeftOfPLatform(Platform& platform);
+void CheckHeadboxIsLeftOfPlatform(Platform& platform);
+
+
+void CameraFollow();
+
+void Draw();
+void DrawPlatforms();
+void DrawPlatformsAABB();
+void DrawAllGameObjectsByTypeRotated(GameObjectType type);
+void DrawAllGameObjectsByType(GameObjectType type);
+void DrawObjectAABB(Point2D objcentre, Vector2D objAABB);
+void DrawPlayerAABB();
+void DrawPlayerNextPositionAABB();
+void DrawDebug();
